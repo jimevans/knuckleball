b56fb4c574c3a62a4c08af6d2c845162d6fb408f
Jim Evans <james.h.evans.jr@gmail.com>
2012-10-25 12:31:59 -0400

    Adding unit test project

---
+++ MP4V2.NET.Tests/MP4FileTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MP4V2.NET.Tests
+{
+    [TestFixture]
+    public class MP4FileTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldNotAllowNullFileName()
+        {
+            MP4File file = new MP4File(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldNotAllowEmptyStringFileName()
+        {
+            MP4File file = new MP4File(string.Empty);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldNotAllowFileNameWhichDoesNotExist()
+        {
+            MP4File file = new MP4File(@"C:\This\Path\Does\Not\Exist\Nor\Does\This\File.m4v");
+        }
+    }
+}
